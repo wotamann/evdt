bfa3a1c377543a76966c0946d9a0f5f62e0a5e29
wotamann <woko@aon.at>
2020-05-12 15:19:52 +0200

    modified working sample

--- src/components/VueForm.vue
+++ src/components/VueForm.vue
@@ -7,52 +7,19 @@
   >
     <template v-slot:top>
       <v-toolbar flat color="white">
-        <v-toolbar-title>My CRUD</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
-        <v-switch v-model="switch1" label="v-form-base"></v-switch>
-        <v-spacer></v-spacer>
+        <v-btn color="primary" dark class="mb-2" @click="addItem">New Item</v-btn>
         <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
-          </template>
           <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
             <v-card-text>
-              <v-container v-if="switch1">
+              <v-container >
                 <v-form-base :model="editedItem" :schema="schema"></v-form-base>
-              </v-container>
-              <v-container v-else>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                  </v-col>
-                </v-row>
               </v-container>
             </v-card-text>
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+              <v-btn color="blue darken-1" text @click="okay">Ok</v-btn>
+              <v-btn color="blue darken-1" text @click="cancel">Cancel</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -79,7 +46,9 @@
   </v-data-table>
 </template>
 <script>
-  import VFormBase from 'vuetify-form-base'
+  import VFormBase from './vFormBase'  // 
+  // import VFormBase from 'vuetify-form-base'
+  
   export default {
     components:{
       VFormBase
@@ -109,13 +78,7 @@
       ],
       desserts: [],
       editedIndex: -1,
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
-      },
+      editedItem: {},
       defaultItem: {
         name: '',
         calories: 0,
@@ -123,19 +86,8 @@
         carbs: 0,
         protein: 0,
       },
+      bufferItemForCanceling:{}
     }),
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-      },
-    },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-    },
 
     created () {
       this.initialize()
@@ -178,71 +130,30 @@
             fat: 16.0,
             carbs: 49,
             protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
+          }
         ]
       },
-
+      addItem(){
+        this.desserts.push({...this.defaultItem})
+        this.editItem(this.desserts[this.desserts.length-1])
+      },
       editItem (item) {
         this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
+        
+        this.editedItem= item
+        this.bufferItemForCanceling = {...item }
         this.dialog = true
       },
-
       deleteItem (item) {
         const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+        this.desserts.splice(index, 1)
       },
-
-      close () {
+      okay () {
         this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
       },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
+      cancel () {
+        this.editedItem = Object.assign(this.editedItem, this.bufferItemForCanceling )       
+        this.dialog = false
       },
     },
   }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+// import '@mdi/font/css/materialdesignicons.min.css' // Ensure you are using css-loader
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  icons: {
+    // https://vuetifyjs.com/en/customization/icons
+    iconfont: 'mdi' // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
+  }
+})
